7f964be9dfaf108a301bb944105bbd4019ac1242
Rusky <91284372+RuskyDev@users.noreply.github.com>
2024-11-03 23:25:53 +0500

    Update script.js

--- script.js
+++ script.js
@@ -128,7 +128,7 @@ async function fetchRepos() {
     const repos = ['convertpng.online', 'discord-bot-template-v14', 'keyboard-sounds', 'PortScanner', 'refreshrate', 'WallMaster', 'Wally'];
     const username = 'RuskyDev';
     const repoContainer = document.getElementById('repos');
-    const do_not_use_this_token = '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';
+    const do_not_use_this_token = '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';
 
     for (let repo of repos) {
         try {

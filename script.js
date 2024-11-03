175f0332efa4a7d7820819a42abd77b8c1d6cb8b
Rusky <iamayaanalee@gmail.com>
2024-11-03 23:17:27 +0500

    added new github token

--- script.js
+++ script.js
@@ -128,7 +128,7 @@ async function fetchRepos() {
     const repos = ['convertpng.online', 'discord-bot-template-v14', 'keyboard-sounds', 'PortScanner', 'refreshrate', 'WallMaster', 'Wally'];
     const username = 'RuskyDev';
     const repoContainer = document.getElementById('repos');
-    const do_not_use_this_token = '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';
+    const do_not_use_this_token = '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';
 
     for (let repo of repos) {
         try {
